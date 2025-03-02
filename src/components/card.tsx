f56f22ec2f8c2c2fff92862becb863852c6cf950
LucasTravessa <lucas.cos.travessa@gmail.com>
2025-03-02 18:43:36 -0300

    fix: update Card component layout and styling for improved presentation

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -15,9 +15,9 @@ export const Card = ({ id, title, picture, price }: Props) => {
   return (
     <Link href={`/details/${id}`} asChild>
       <Pressable>
-        <View className="m-2 overflow-hidden rounded-xl border border-neutral-300 bg-white  dark:bg-neutral-900">
+        <View className="m-2 h-96 w-[45vw] overflow-hidden rounded-xl border border-neutral-300 bg-white  dark:bg-neutral-900">
           <Image
-            className="h-56 w-full overflow-hidden rounded-t-xl"
+            className="h-1/2 w-full overflow-hidden rounded-t-xl"
             contentFit="cover"
             source={{
               uri: picture || imagePlaceHolder,
@@ -25,8 +25,12 @@ export const Card = ({ id, title, picture, price }: Props) => {
           />
 
           <View className="p-2">
-            <Text className="py-3 text-2xl ">{title}</Text>
-            <Text className="py-3 text-2xl ">R$ {price.toFixed(2)}</Text>
+            <Text numberOfLines={2} className="py-3 text-2xl font-semibold">
+              {title}
+            </Text>
+            <Text className="py-3 text-2xl font-bold">
+              R$ {price.toFixed(2)}
+            </Text>
             <Rating rating={3} />
           </View>
         </View>
