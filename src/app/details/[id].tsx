5969c18d822bb911453ff392872e1c8bbe018360
LucasTravessa <lucas.cos.travessa@gmail.com>
2025-03-02 18:44:50 -0300

    fix: enhance product details page layout and error messaging

--- src/app/details/[id].tsx
+++ src/app/details/[id].tsx
@@ -1,5 +1,4 @@
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { ScrollView } from 'moti';
 import * as React from 'react';
 
 import { useProduct } from '@/api/products';
@@ -14,6 +13,7 @@ import {
   Text,
   View,
 } from '@/components/ui';
+import { useCartStore } from '@/lib/hooks/use-cart';
 
 const imagePlaceHolder =
   'https://images.unsplash.com/photo-1489749798305-4fea3ae63d43?auto=format&fit=crop&w=800&q=80';
@@ -27,11 +27,11 @@ export default function Details() {
     variables: { id: local.id },
   });
 
-  // const store = useCartStore();
+  const store = useCartStore();
 
   const handleAddToCart = () => {
-    // store.removeFromCart(data.id);
-    // store.addToCart(data, quantity);
+    if (!!!data) return;
+    store.addToCart(data, quantity);
   };
 
   if (isPending) {
@@ -48,7 +48,7 @@ export default function Details() {
       <View className="flex-1 justify-center p-3">
         <Stack.Screen />
         <FocusAwareStatusBar />
-        <Text className="text-center">Error loading post</Text>
+        <Text className="text-center">Error loading the Product</Text>
       </View>
     );
   }
@@ -57,24 +57,25 @@ export default function Details() {
     <View className="flex-1 p-3 ">
       <Stack.Screen />
       <FocusAwareStatusBar />
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View className="mx-4 my-6 pt-3">
-          <Text className="font-titilium-black text-neutral pt-1 text-3xl">
-            {data?.title}
-          </Text>
+      <View>
+        <View className="my-2">
+          <Text className="mb-2 text-3xl font-bold">{data?.title}</Text>
           <Rating rating={3} />
         </View>
-        <View className="mx-4 mt-2 flex flex-col items-center justify-center px-4 py-2">
+        <View className="mb-8 mt-2 flex flex-col items-center justify-center">
           <Image
-            source={data.picture || imagePlaceHolder}
             className="size-96 rounded-xl object-contain"
+            contentFit="cover"
+            source={{
+              uri: data.picture || imagePlaceHolder,
+            }}
           />
         </View>
         <ColorPicker />
-        <Text className="font-titilium-black text-neutral pt-1 text-3xl">
+        <Text className="mt-5 text-3xl font-bold">
           R$ {data.price.toFixed(2)}
         </Text>
-        <View className="mx-4 mt-2 flex flex-row justify-between px-4 py-2">
+        <View className="mt-8 flex flex-row justify-between">
           <QuantityPicker
             quantity={quantity}
             onIncrease={() => setQuantity(quantity + 1)}
@@ -82,7 +83,7 @@ export default function Details() {
           />
           <Button label="Adicionar ao carrinho" onPress={handleAddToCart} />
         </View>
-      </ScrollView>
+      </View>
     </View>
   );
 }
