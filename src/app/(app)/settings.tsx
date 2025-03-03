5a8a1a2690d5a2cdb612d0ef9a40bd96c84333a3
LucasTravessa <lucas.cos.travessa@gmail.com>
2025-03-03 14:13:46 -0300

    feat: enhance settings functionality with cleanup and navigation improvements

--- src/app/(app)/settings.tsx
+++ src/app/(app)/settings.tsx
@@ -1,15 +1,25 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-import { useRouter } from 'expo-router';
+import { router } from 'expo-router';
 
 import { Item } from '@/components/settings/item';
 import { ItemsContainer } from '@/components/settings/items-container';
 import { FocusAwareStatusBar, ScrollView, View } from '@/components/ui';
-import { useAuth } from '@/lib';
+import { openLinkInBrowser, useAuth } from '@/lib';
+import { useCartStore } from '@/lib/hooks/use-cart';
+import { useCheckoutStore } from '@/lib/hooks/use-checkout';
 
 export default function Settings() {
   const signOut = useAuth.use.signOut();
-  const router = useRouter();
+  const { clearCart } = useCartStore();
+  const { clearCheckout } = useCheckoutStore();
+  function cleanUpApp() {
+    clearCart();
+    clearCheckout();
+    signOut();
+  }
+  const apiDocsUrl = 'http://localhost:3000/doc#/';
+
   return (
     <>
       <FocusAwareStatusBar />
@@ -17,8 +27,18 @@ export default function Settings() {
       <ScrollView>
         <View className="flex-1 px-4 pt-16 ">
           <ItemsContainer title="TEST FLOWS">
-            <Item text="Adicionar ao fluxo do carrinho" onPress={() => {}} />
-            <Item text="Checkout Flow" onPress={() => {}} />
+            <Item
+              text="Adicionar ao fluxo do carrinho"
+              onPress={() => {
+                router.push('/');
+              }}
+            />
+            <Item
+              text="Checkout Flow"
+              onPress={() => {
+                router.push('/cart');
+              }}
+            />
             <Item
               text="Log In Flow"
               onPress={() => {
@@ -32,13 +52,16 @@ export default function Settings() {
           <ItemsContainer title="ACTIONS">
             <ItemsContainer>
               <Item text="Log out" onPress={signOut} />
-              <Item text="Reset App State" onPress={() => {}} />
+              <Item text="Reset App State" onPress={cleanUpApp} />
             </ItemsContainer>
           </ItemsContainer>
 
           <ItemsContainer title="OTHER">
             <ItemsContainer>
-              <Item text="API Calls" onPress={() => {}} />
+              <Item
+                text="API Calls"
+                onPress={() => openLinkInBrowser(apiDocsUrl)}
+              />
               <Item text="Report A Bug" onPress={() => {}} />
               <Item text="About" onPress={() => {}} />
             </ItemsContainer>
