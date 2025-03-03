8d77387ff9479253073b4a807dde7a2fe07dca1f
LucasTravessa <lucas.cos.travessa@gmail.com>
2025-03-03 13:03:21 -0300

    feat: replace shopping cart icon with gradient checkmark circle in checkout completion screen

--- src/app/(app)/checkout/complete.tsx
+++ src/app/(app)/checkout/complete.tsx
@@ -1,16 +1,16 @@
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { router } from 'expo-router';
 import React from 'react';
 import { View } from 'react-native';
 
 import { BlueBlurBg } from '@/components/blue-blur-bg';
 import { Button, Text } from '@/components/ui';
+import { GradientCheckmarkCircle } from '@/components/ui/icons/gradient-checkmark-circle';
 
 export default function Complete() {
   return (
     <BlueBlurBg>
       <View className="mx-4 mt-32 flex-1 items-center justify-start">
-        <MaterialIcons name="shopping-cart" size={90} color="lightgray" />
+        <GradientCheckmarkCircle />
         <Text className="mt-10 text-3xl font-bold">Checkout completado</Text>
         <Text className="my-8 text-center text-lg">
           Obrigado pelo seu pedido. Seu pedido foi despachado e chegará tão
